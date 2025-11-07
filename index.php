eba947844fe75db163b5b4c9e0b51a9a75946027
Elena Volynkina <vollnlenna57@gmail.com>
2025-11-07 22:18:38 +0300

    подключение стилей

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Кадровый учет сотрудников организации</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <header class="main-header">
